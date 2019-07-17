3ceaa1582ce64ce1d148e82f8ba779700b99ab0d
fanzhe <kvy007@qq.com>
2019-07-17 14:08:09 +0800

    更新了Optimization.ipynb文件,里面包含常见的加快训练的方法; 更新了network.py文件,里面包含一个简单的双层神经网络,可以用来测试;

---
+++ SumLearn/Models/relate/network.py
@@ -0,0 +1,144 @@
+import numpy as np
+from collections import OrderedDict
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+    def forward(self,x):
+#         print(x)
+        self.mask = (x<=0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+    def backward(self,dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x,self.W) + self.b
+        return out
+    def backward(self, dout):
+        dx = np.dot(dout,self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout,axis=0)
+        return dx
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+    def forward(self,x,t):
+        self.t = t
+        self.y = self.softmax(x)
+        self.loss = self.cross_entropy_error(self.y,self.t)
+        return self.loss
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y -self.t)/batch_size
+        return dx
+    def cross_entropy_error(self, y, t):
+        if y.ndim == 1:
+            t = t.reshape(1, t.size)
+            y = y.reshape(1, y.size)
+        # 监督数据是one-hot-vector的情况下，转换为正确解标签的索引
+        if t.size == y.size:
+            t = t.argmax(axis=1)
+        batch_size = y.shape[0]
+        return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+    def softmax(self, x):
+        # 适用于多批量数据的softmax
+        if x.ndim == 2:
+            x = x.T
+            x = x - np.max(x, axis=0)
+            y = np.exp(x) / np.sum(np.exp(x), axis=0)
+            return y.T
+        x = x - np.max(x) # 溢出对策
+        return np.exp(x) / np.sum(np.exp(x))
+class SimpleNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+        # 生成层
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'],
+                                        self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'],
+                                        self.params['b2'])
+        self.lastLayer = SoftmaxWithLoss()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
+        acc = np.sum(y == t) / float(x.shape[0])
+        return acc
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        grads = {}
+        grads['W1'] = self.numerical_gradient_mini(loss_W, self.params['W1'])
+        grads['b1'] = self.numerical_gradient_mini(loss_W, self.params['b1'])
+        grads['W2'] = self.numerical_gradient_mini(loss_W, self.params['W2'])
+        grads['b2'] = self.numerical_gradient_mini(loss_W, self.params['b2'])
+        return grads
+
+    # 适用于mini-batch的梯度算法
+    def numerical_gradient_mini(self, f, x):
+        h = 1e-4  # 0.0001
+        grad = np.zeros_like(x)
+        it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+        while not it.finished:
+            idx = it.multi_index
+            tmp_val = x[idx]
+            x[idx] = float(tmp_val) + h
+            fxh1 = f(x)  # f(x+h)
+            x[idx] = tmp_val - h
+            fxh2 = f(x)  # f(x-h)
+            grad[idx] = (fxh1 - fxh2) / (2 * h)
+            x[idx] = tmp_val  # 还原值
+            it.iternext()
+        return grad
+
+    # 反向传播的梯度算法
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+        # backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        # 获取当前网络的最新反向传播数据
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+        return grads
+
+if __name__ == '__main__':
+    net = SimpleNet(64,50,10)
