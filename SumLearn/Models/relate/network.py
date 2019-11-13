cafdee78368985308ea9bd6c36351da8ae7be1b5
fanzhe <kvy007@qq.com>
2019-11-13 18:09:46 +0800

    更新了一堆东西哈哈

--- SumLearn/Models/relate/network.py
+++ SumLearn/Models/relate/network.py
@@ -14,22 +14,7 @@ class Relu:
         dout[self.mask] = 0
         dx = dout
         return dx
-# class Affine:
-#     def __init__(self, W, b):
-#         self.W = W
-#         self.b = b
-#         self.x = None
-#         self.dW = None
-#         self.db = None
-#     def forward(self, x):
-#         self.x = x
-#         out = np.dot(x,self.W) + self.b
-#         return out
-#     def backward(self, dout):
-#         dx = np.dot(dout,self.W.T)
-#         self.dW = np.dot(self.x.T, dout)
-#         self.db = np.sum(dout,axis=0)
-#         return dx
+
 
 # 适用于多个张量运算的Affine
 class Affine:
